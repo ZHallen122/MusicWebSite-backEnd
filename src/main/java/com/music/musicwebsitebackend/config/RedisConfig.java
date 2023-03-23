c5f94aac19ce3fcca869c1ae09dca82b2a8c8811
ZHallen122 <zhallen12261@gmail.com>
2023-03-23 13:17:38 -0400

    update config

--- src/main/java/com/music/musicwebsitebackend/config/RedisConfig.java
+++ src/main/java/com/music/musicwebsitebackend/config/RedisConfig.java
@@ -13,47 +13,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * RedisConfig
+ * */
 @Configuration
 @EnableCaching
 public class RedisConfig  extends CachingConfigurerSupport {
 
-
+    /**
+     * change the redisTemplate default key serializer to StringRedisSerializer
+     * */
     @Bean
     public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
         RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
-        //默认的Key序列化器为: JdkSerializationRedisSerializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory( connectionFactory) ;
         return redisTemplate;
     }
 
-//        @Value("${spring.redis.host}")
-//        private String redisHost;
-//
-//        @Value("${spring.redis.port}")
-//        private int redisPort;
-//
-//        @Bean
-//        public LettuceConnectionFactory lettuceConnectionFactory() {
-//            RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-//            // Uncomment and set the password if your Redis instance requires authentication
-//            // redisStandaloneConfiguration.setPassword(RedisPassword.of("your_redis_password"));
-//            return new LettuceConnectionFactory(redisStandaloneConfiguration);
-//        }
-//
-//        @Bean
-//        public RedisTemplate<String, Object> redisTemplate() {
-//            RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//            redisTemplate.setConnectionFactory(lettuceConnectionFactory());
-//            // Customize the RedisTemplate as needed
-//            return redisTemplate;
-//        }
-//
-//        @Bean
-//        public StringRedisTemplate stringRedisTemplate() {
-//            StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-//            stringRedisTemplate.setConnectionFactory(lettuceConnectionFactory());
-//            // Customize the StringRedisTemplate as needed
-//            return stringRedisTemplate;
-//        }
 }
