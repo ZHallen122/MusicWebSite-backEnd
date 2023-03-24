56cc39461acb3ae220c2ff36e74460c852185727
ZHallen122 <zhallen12261@gmail.com>
2023-03-23 23:22:25 -0400

    update Login and sign up

--- src/main/java/com/music/musicwebsitebackend/controller/UserController.java
+++ src/main/java/com/music/musicwebsitebackend/controller/UserController.java
@@ -8,6 +8,7 @@ import com.music.musicwebsitebackend.service.UserService;
 import com.music.musicwebsitebackend.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -29,10 +30,12 @@ public class UserController {
     @PostMapping("/login")
     public Result userLogin(@RequestParam("email") String email, @RequestParam("password")String password) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         User user = userService.selectByUserName(email);
-        //login fail
-        if(user == null || !user.getPassword().equals(password)){
+
+        // Login fail if user not found or passwords don't match
+        if(user == null || !new BCryptPasswordEncoder().matches(password, user.getPassword())) {
             return Result.error("Login fail");
         }else{
+            // Generate token for user authentication
             Map<String, Object> map = new HashMap<>();
             map.put("role",user.getRole());
             String id = " "+user.getUser_id();
@@ -44,6 +47,11 @@ public class UserController {
     @PostMapping("/signup")
     public Result signUp(@RequestBody User user){
         user.setRole("user");
+
+        // Hash the user's password using BCryptPasswordEncoder
+        String encodedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         Boolean check = userService.signUp(user);
         if(check){
             return Result.success("sign up success");
